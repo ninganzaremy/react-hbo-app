0f25bc5bbe47f6503a46b584302172c065396dee
Ningaanza Remy <remimaradona@gmail.com>
2022-01-05 17:35:06 -0700

    created and styled login Page

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,8 @@ class MyDocument extends Document {
 				<Head>
 					<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" />
 
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
-					<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;300;700&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
 					<Main />
--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,25 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return <h1>Test</h1>;
+	return (
+		<div>
+			<div className="login-user">
+				<div className="login-user__top">
+					<div className="login-user__logo" />
+					<span className="login-user__title">Who Is Watching?</span>
+				</div>
+
+				<div className="login-user__form">
+					<div className="login-user__user-box">
+						<img src="https://loremfaces.com/#gallery-5" className="login-user__user-img" alt="Picture of the player" />
+						<div className="login-user__user-name">Remix</div>
+					</div>
+				</div>
+				<div className="login-user__buttons">
+					<button className="login-user__adult">Add Adult</button>
+					<button className="login-user__kid">Add Kid</button>
+				</div>
+			</div>
+		</div>
+	);
 }
