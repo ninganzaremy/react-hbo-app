b66ba1876c729166020402f920686ab3d64f268c
Ningaanza Remy <remimaradona@gmail.com>
2022-01-11 22:29:38 -0700

    Create Featured Media Page and styled it

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import MainLayout from "../components/Layouts/MainLayout";
+import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
 
 export default function HomeView() {
 	return (
