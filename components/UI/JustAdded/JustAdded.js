a339c816a64176d034731461d715888ecfcc2da0
Ningaanza Remy <remimaradona@gmail.com>
2022-01-13 22:00:22 -0700

    created and styled Components JustAdded and PosterView

--- components/UI/JustAdded/JustAdded.js
+++ components/UI/JustAdded/JustAdded.js
@@ -1,4 +1,4 @@
-const ForYouList = (props) => {
+const JustAdded = (props) => {
 	const loopComp = (comp, digit) => {
 		let thumbnails = [];
 		for (let index = 1; index <= digit; index++) {
@@ -8,13 +8,13 @@ const ForYouList = (props) => {
 		return thumbnails;
 	};
 	return (
-		<div className="foryou-list">
-			<h3 className="foryou-list__title">For You</h3>
-			<div className="foryou-list__thumbnails">
+		<div className="just-added">
+			<h3 className="just-added__title">Just Added</h3>
+			<div className="just-added__thumbnails">
 				{loopComp(
-					<div className="foryou-list__thumbnail">
+					<div className="just-added__thumbnail">
 						<img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2?imbypass=on" />
-						<div className="foryou-list__top-layer">
+						<div className="just-added__top-layer">
 							<i className="fas fa-play" />
 						</div>
 					</div>,
@@ -25,4 +25,4 @@ const ForYouList = (props) => {
 	);
 };
 
-export default ForYouList;
+export default JustAdded;
