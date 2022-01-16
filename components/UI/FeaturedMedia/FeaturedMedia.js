a8fb5bacdfcfad70236effcb4d7c03295ad0f09f
Ningaanza Remy <remimaradona@gmail.com>
2022-01-16 11:15:27 -0700

    fixed the DOM property name typo in FeaturedMedia Component

--- components/UI/FeaturedMedia/FeaturedMedia.js
+++ components/UI/FeaturedMedia/FeaturedMedia.js
@@ -1,7 +1,7 @@
 const FeaturedMedia = (props) => {
 	return (
 		<div className="featured-media">
-			<iframe className="featured-media__video" width="100%" height="100%" src="https://www.youtube.com/embed/NYH2sLid0Zc?autoplay=1&loop=1&start=16" allow="accelerometer; autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+			<iframe className="featured-media__video" width="100%" height="100%" src="https://www.youtube.com/embed/NYH2sLid0Zc?autoplay=1&loop=1&start=16" allow="accelerometer; autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 
 			<div className="featured-media__bg">
 				<div className="featured-media__container">
